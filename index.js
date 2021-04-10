a3f17e3e607a682ac2ade606c2ffcb9c9f38ebfa
Evan Brandt <evanmbrandt@gmail.com>
2021-04-09 18:22:03 -0700

    added coordinate click

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const tmi = require('tmi.js');
 const puppeteer = require('puppeteer');
 
+let browser = null;
+let page = null;
+let instrumentCanvasElement = null;
+
 const client = new tmi.Client({
   connection: {
     secure: true,
@@ -12,13 +16,27 @@ const client = new tmi.Client({
 client.connect();
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto('http://okayco.de');
-  
-    await browser.close();
+    browser = await puppeteer.launch(
+      { 
+        headless: false,
+        args: [`--window-size=1980,1080`]
+      });
+    page = await browser.newPage();
+    await page.setViewport({ width: 1950, height: 950});
+    await page.goto('https://musiclab.chromeexperiments.com/Song-Maker',{waitUntil: 'networkidle0'});
+    musicCanvasElement = await page.$("#instrument-canvas");
   })();
 
 client.on('message', (channel, tags, message, self) => {
   console.log(`${tags['display-name']}: ${message}`);
+  if(message.toLowerCase().startsWith('!note')){
+    const args = message.split(' ');
+    const x = parseInt(args[1]);
+    const y = parseInt(args[2]);
+    clickPage(x, y);
+  }
 });
+
+async function clickPage(x, y){
+  await page.mouse.click(x,y);
+}
